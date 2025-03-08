29786d260345b215c9b92567898901e6f5d91f66
icey-rain <izzyrein@gmail.com>
2025-03-08 21:28:32 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 05

--- CircleCalculator.java
+++ CircleCalculator.java
@@ -4,12 +4,13 @@ public class CircleCalculator {
 Scanner input = new Scanner(System.in);
 System.out.print("Enter the radius of the circle: ");
 double radius = input.nextDouble();
-double area = Math.PI * Math.pow(radius, 2);double area = Math.PI * Math.pow(radius, 2);
+double area = Math.PI * Math.pow(radius, 2);
 double circumference = 2 * Math.PI * radius;
 System.out.printf("Radius: %.2f%n", radius);
 System.out.printf("Area: %.2f%n", area);
 System.out.printf("Circumference: %.2f%n", circumference);
 input.close();
+
 
     }
 }
